2dd540d65b0cee09011dfceb2d4131ed7f419179
Samuele Corrà <nabi.samuel.corra@gmail.com>
2026-08-04 03:12:01 +0200

    python

---
+++ lessons/cybersecurity/extra/python_corso_completo/l1.py
@@ -0,0 +1,16 @@
+# Questo è il mio primo programma in Python!
+
+# Per i commenti multiline, bisogna usare le triple virgolette (""").
+
+"""
+Tutto
+questo
+è
+un commento
+Ma all'infuori dei commenti, che vengono ignorati dall'interprete, cosa scriveremo?
+"""
+
+print("I veri programmatori iniziano con C, perché fa capire meglio come funziona un computer.")
+print("Ma è anche vero che gestire un LLM in C non sarebbe una buona idea, quindi useremo Python.")
+
+
